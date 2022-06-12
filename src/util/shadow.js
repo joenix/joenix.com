e83c747d83e3d69550fad017ad6e8b3b2cc65b6e
joenix <joenix.com@gmail.com>
2022-06-12 12:36:47 +0800

    feat: shadow

--- src/util/shadow.js
+++ src/util/shadow.js
@@ -1,6 +1,46 @@
+function shadow(target, { screen, mouse }) {
+  // Get Context Screen
+  const view = {
+    w: target.clientWidth,
+    h: target.clientHeight,
+  }
+
+  // Get Target Center
+  const center = {
+    x: target.offsetLeft + view.w / 2,
+    y: target.offsetTop + view.h / 2,
+  }
+
+  // Get Ratio
+  const divisor = {
+    x: screen.w / (screen.w - view.w),
+    y: screen.h / (screen.h - view.h),
+  }
+
+  // Get Average
+  const average = {
+    x: Math.abs(mouse.x - center.x),
+    y: Math.abs(mouse.y - center.y),
+  }
+
+  // Get Sin
+  const sin = Math.sqrt(Math.pow(average.x, 2) + Math.pow(average.y, 2))
+
+  // Get Style
+  const style = {
+    x: `${(mouse.x - center.x) / -divisor.x}px`,
+    y: `${(mouse.y - center.y) / -divisor.y}px`,
+    s: `${sin / divisor.x / 12}px`,
+    c: `rgba(0, 0, ${sin / divisor.y / 12}, .1)`,
+  }
+
+  // Set Style
+  target.style.boxShadow = `${style.x} ${style.y} ${style.s} ${style.c}`
+}
+
 export default event => {
   // Get Context Node
-  const context = document.querySelector('.context')
+  const context = document.querySelectorAll('.context > a')
 
   if (event.touches) {
     event = event.touches[0]
@@ -18,38 +58,8 @@ export default event => {
     h: document.body.clientHeight,
   }
 
-  // Get Ratio
-  const divisor = {
-    x: screen.w / (screen.w - 640),
-    y: screen.h / (screen.h - 360),
+  // Shadow Method
+  for (const target of context) {
+    shadow(target, { screen, mouse })
   }
-
-  // Get Screen Center
-  const center = {
-    x: screen.w / divisor.x,
-    y: screen.h / divisor.y,
-  }
-
-  // Get Average
-  const average = {
-    x: Math.abs(mouse.x - center.x),
-    y: Math.abs(mouse.y - center.y),
-  }
-
-  // Get Sin
-  const sin = Math.sqrt(Math.pow(average.x, 2) + Math.pow(average.y, 2))
-
-  // Get Ratio
-  const ratio = 4
-
-  // Get Style
-  const style = {
-    x: `${(mouse.x - center.x) / -ratio}px`,
-    y: `${(mouse.y - center.y) / -ratio}px`,
-    s: `${sin / ratio / 12}px`,
-    c: `rgba(0, 0, ${sin / ratio / 12}, .2)`,
-  }
-
-  // Set Style
-  context.style.textShadow = `${style.x} ${style.y} ${style.s} ${style.c}`
 }
